15afd33c8e601a6ee6a98f37fbd91a69bf0a2b48
ndebaumann <ndebaumann@gmail.com>
2022-03-18 16:40:07 +0100

    Reduce number of training steps for testing

--- src/auto_setup/write_exp_config_json.py
+++ src/auto_setup/write_exp_config_json.py
@@ -19,7 +19,7 @@ data = {
     "TEST_CSV": "latest",
     "EVAL_CONF": 0.5,
     "RUN_PARAMS": {
-        "STEPS": 100000
+        "STEPS": 1000
     },
     "MODEL_PARAMS": {
         "BASE_MODEL": "faster_rcnn_resnet50_v1_640x640_coco17_tpu-8",
