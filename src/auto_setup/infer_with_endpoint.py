af6a445ca73a43e4a130026760b56bbc11f3f3a4
Daniel Baumann <ndebaumann@gmail.com>
2022-04-04 15:37:02 +0200

    Update inference script to be pipeline ready

--- src/auto_setup/infer_with_endpoint.py
+++ src/auto_setup/infer_with_endpoint.py
@@ -1,42 +1,49 @@
+from datetime import datetime
+from PIL import Image
+import argparse
 import json
 import os
 import pandas as pd
-from PIL import Image
 import PIL
 import requests
 
-endpoint = 'http://278dd78f-fb02-4321-8dfe-8bae23dcb3f9.westeurope.azurecontainer.io/score?prob=0.5'
-api_key = 'znixQQu5lqsKCWhulYNJzNUsf41ykbPV'
-image_directory = 'synthetic_dataset/images/'
+parser = argparse.ArgumentParser()
+parser.add_argument('-e', '--endpoint', help='AML deployment endpoint for inference')
+parser.add_argument('-k', '--key', help='API key for the AML deployment')
+parser.add_argument('-d', '--dataset', help='Name of dataset/project')
+args = parser.parse_args()
+dataset = args.dataset
+
+image_directory = f'{dataset}/test_images/'
+inferences_directory = f'{dataset}/test_inferences/'
+os.makedirs(inferences_directory, exist_ok=True)
 enable_auth = False
 row_data = []
 counter = 0
 for image_filename in os.listdir(image_directory):
     if image_filename.endswith('.jpg'):
-        if counter >= 5:
-            break
-        counter += 1
         print(image_filename)
         image_PIL = PIL.Image.open(image_directory+image_filename)
         width, height = image_PIL.size
         img = open(image_directory+image_filename, 'rb').read()
-        headers = {'Content-Type': 'application/json', 'Authorization': ('Bearer ' + api_key)}
-        resp = requests.post(endpoint, img, headers=headers)
+        headers = {'Content-Type': 'application/json', 'Authorization': ('Bearer ' + args.key)}
+        resp = requests.post(f'{args.endpoint}?prob=0.5', img, headers=headers)
         results = resp.text
         detections = json.loads(results)
-        for detect in detections:
-            box = detect['bounding_box']
+        for detection in detections:
+            box = detection['bounding_box']
             ymin, xmin, ymax, xmax = box[0], box[1], box[2], box[3]
             row_data.append([image_filename,
                              int(xmin*width),
                              int(xmax*width),
                              int(ymin*height),
                              int(ymax*height),
-                             detect['label'],
-                             detect['confidence']])
+                             detection['label'],
+                             detection['confidence']])
 
 df = pd.DataFrame(row_data)
 column_names = ['filename', 'xmin', 'xmax', 'ymin', 'ymax', 'class', 'Confidence']
 df.columns = column_names
-df.to_csv('labels.csv', index=False)
+time_stamp = datetime.now().strftime('%y%m%d%H%m%S')
+df.to_csv(f'{inferences_directory}/labels_{time_stamp}.csv', index=False)
 print("Complete")
