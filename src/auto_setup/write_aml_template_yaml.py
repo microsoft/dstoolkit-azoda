ccc9b333171eadba4f13131bca65e80db19a35bb
Daniel Baumann <ndebaumann@gmail.com>
2022-04-06 12:03:10 +0200

    Add config arguments to aml config creation

--- src/auto_setup/write_aml_template_yaml.py
+++ src/auto_setup/write_aml_template_yaml.py
@@ -3,13 +3,15 @@ import yaml
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-s', '--subscription', help='Your Azure subscription')
+parser.add_argument('-r', '--resource_group', help='Your Azure resource group')
+parser.add_argument('-w', '--workspace', help='Your AML workspace')
 args = parser.parse_args()
 
-container_reg = f'/subscriptions/{args.subscription}/resourceGroups/tfod-dev-rg/' \
+container_reg = f'/subscriptions/{args.subscription}/resourceGroups/{args.resource_group}/' \
                 f'providers/Microsoft.ContainerRegistry/registries/tfod-dev-acr-{args.subscription}'
 
 config_dict = {'$schema': 'https://azuremlschemas.azureedge.net/latest/workspace.schema.json',
-               'name': 'tfod-dev-amlw',
+               'name': args.workspace,
                'location': 'westeurope',
                'display_name': 'Basic workspace',
                'description': '',
