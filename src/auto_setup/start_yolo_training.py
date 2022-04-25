636e4cf3f7f0961f2f96ba3cb84e97057ee37204
Daniel Baumann <ndebaumann@gmail.com>
2022-04-25 18:01:02 +0200

    Generate yolo yaml based on dataset input

--- src/auto_setup/start_yolo_training.py
+++ src/auto_setup/start_yolo_training.py
@@ -1,18 +1,24 @@
-# %%
 from azureml.core import Workspace, Environment, Experiment, ScriptRunConfig, Dataset
 from datetime import datetime
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--dataset", type=str, help="Dataset name")
+args = parser.parse_args()
+
 ws = Workspace.from_config()
 time_stamp = datetime.now().strftime('%y%m%d_%H%m%S')
 env = Environment.get(workspace=ws, name="AzureML-sklearn-1.0-ubuntu20.04-py38-cpu")
 env = env.clone("yolo_env")
 datastore = ws.get_default_datastore()
-dataset = Dataset.File.from_files(path=(datastore, 'synthetic_dataset/'))
+dataset_name = args.dataset
+dataset = Dataset.File.from_files(path=(datastore, f'{dataset_name}/'))
 
 src = ScriptRunConfig(source_directory='models/yolo/',
                       script='train_coordinator.py',
                       compute_target='gpu-cluster',
                       environment=env.from_pip_requirements('myenv', 'yolo_requirements.txt'),
-                      arguments=['--dataset', 'synthetic_dataset',
+                      arguments=['--dataset', dataset_name,
                                  '--cfg', 'yolov5s.yaml',
                                  '--batch-size', '16',
                                  '--epochs', '5',
