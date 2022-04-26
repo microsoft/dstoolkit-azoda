79f32d10b644a46d01a6b29dd4e2ce0b2cab2bc2
Daniel Baumann <ndebaumann@gmail.com>
2022-04-26 09:28:33 +0200

    Test msi auth

--- src/auto_setup/start_yolo_training.py
+++ src/auto_setup/start_yolo_training.py
@@ -1,12 +1,23 @@
+# %%
 from azureml.core import Workspace, Environment, Experiment, ScriptRunConfig, Dataset
+from azureml.core.authentication import MsiAuthentication
 from datetime import datetime
 import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--dataset", type=str, help="Dataset name")
+parser.add_argument("--subscription_id", type=str, help="Subscription ID")
+parser.add_argument("--resource_group", type=str, help="Resource Group")
+parser.add_argument("--workspace_name", type=str, help="Workspace name")
 args = parser.parse_args()
 
-ws = Workspace.from_config()
+msi_auth = MsiAuthentication()
+# ws = Workspace.from_config()
+ws = Workspace(subscription_id=args.subscription_id,
+               resource_group=args.resource_group,
+               workspace_name=args.workspace_name,
+               auth=msi_auth)
+
 time_stamp = datetime.now().strftime('%y%m%d_%H%m%S')
 env = Environment.get(workspace=ws, name="AzureML-sklearn-1.0-ubuntu20.04-py38-cpu")
 env = env.clone("yolo_env")
