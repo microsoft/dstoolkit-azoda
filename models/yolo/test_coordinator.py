4fd940bbc50e9bfa2730f2d45ca49721ee7743d8
Daniel Baumann <ndebaumann@gmail.com>
2022-04-26 14:35:08 +0200

    Adds remote test pipeline and cleanup

--- models/yolo/test_coordinator.py
+++ models/yolo/test_coordinator.py
@@ -2,18 +2,22 @@ import argparse
 import os
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--images", type=str, help="Images for inference")
+parser.add_argument("--dataset", type=str, help="Dataset name")
 parser.add_argument("--weights", type=str, help="Path to model weights")
 parser.add_argument("--conf", type=str, help="Confidence threshold")
+parser.add_argument("--iou", type=str, help="Bounding box IOU threshold")
 args = parser.parse_args()
 
 # project must be set to outputs, since the AML saves results in the outputs directory
-print('Start inferencing')
-os.system(f"python yolov5/detect.py \
+print('Start testing')
+os.system(f"python yolov5/val.py \
           --conf-thres {args.conf} \
+          --data {args.dataset}.yaml \
+          --iou-thres {args.iou} \
           --project outputs/ \
           --save-conf \
           --save-txt \
-          --source {args.images} \
+          --task val \
+          --verbose \
           --weights {args.weights}")
-print('Inferencing complete')
+print('Testing complete')
