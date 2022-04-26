354380f544675cd6a38685db3afaefb32761cdb2
Daniel Baumann <ndebaumann@gmail.com>
2022-04-26 10:16:52 +0200

    Adds variable group and spn auth

--- src/auto_setup/start_yolo_training.py
+++ src/auto_setup/start_yolo_training.py
@@ -1,23 +1,25 @@
-# %%
 from azureml.core import Workspace, Environment, Experiment, ScriptRunConfig, Dataset
-from azureml.core.authentication import MsiAuthentication
+from azureml.core.authentication import ServicePrincipalAuthentication
 from datetime import datetime
 import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--dataset", type=str, help="Dataset name")
+parser.add_argument("--tenant_id", type=str, help="Tenant ID")
+parser.add_argument("--service_principal_id", type=str, help="Service Principal ID")
+parser.add_argument("--service_principal_password", type=str, help="Serice Principal Password")
 parser.add_argument("--subscription_id", type=str, help="Subscription ID")
 parser.add_argument("--resource_group", type=str, help="Resource Group")
-parser.add_argument("--workspace_name", type=str, help="Workspace name")
+parser.add_argument("--workspace_name", type=str, help="Workspace Name")
 args = parser.parse_args()
 
-msi_auth = MsiAuthentication()
-# ws = Workspace.from_config()
-ws = Workspace(subscription_id=args.subscription_id,
-               resource_group=args.resource_group,
-               workspace_name=args.workspace_name,
-               auth=msi_auth)
-
+sp = ServicePrincipalAuthentication(tenant_id=args.tenant_id,
+                                    service_principal_id=args.service_principal_id,
+                                    service_principal_password=args.service_principal_password)
+ws = Workspace.get(subscription_id=args.subscription_id,
+                   resource_group=args.resource_group,
+                   name=args.workspace_name,
+                   auth=sp)
 time_stamp = datetime.now().strftime('%y%m%d_%H%m%S')
 env = Environment.get(workspace=ws, name="AzureML-sklearn-1.0-ubuntu20.04-py38-cpu")
 env = env.clone("yolo_env")
