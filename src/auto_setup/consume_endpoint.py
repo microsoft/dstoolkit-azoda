ef9977eb244fba15c1547ba37680e0abbc6d9ba2
Daniel Baumann <ndebaumann@gmail.com>
2022-09-19 18:06:00 +0200

    Added robustness

--- src/auto_setup/consume_endpoint.py
+++ src/auto_setup/consume_endpoint.py
@@ -6,23 +6,27 @@ import cv2
 import json
 import requests
 
-sample_image_path = 'path_to_img'
+sample_image_path = "img_0.jpg"
 # Get this information from your AML endpoint under the Consume tab
-endpoint = 'your_endpoint'
-api_key = 'your_key'
-headers = {'Content-Type': 'application/json', 'Authorization': ('Bearer ' + api_key)}
-with open(sample_image_path, mode='rb') as file:
+endpoint = "your_endpoint"
+api_key = "your_key"
+headers = {"Content-Type": "application/json", "Authorization": ("Bearer " + api_key)}
+with open(sample_image_path, mode="rb") as file:
     img = file.read()
 data = dict()
-data['img'] = base64.encodebytes(img).decode('utf-8')
+data["img"] = base64.encodebytes(img).decode("utf-8")
 json_string = json.dumps(data)
 response = requests.post(endpoint, data=json_string, headers=headers)
-extra_texts = ['tensor', ', device=\'cuda:0\'']
+extra_texts = ["tensor", ", device='cuda:0'"]
 response_str = response.json()
 for extra_text in extra_texts:
-    response_str = response_str.replace(extra_text, '')
-print(f'Response: {response_str})')
-bbs = ast.literal_eval(response_str)
+    response_str = response_str.replace(extra_text, "")
+print(f"Response: {response_str})")
+if response_str == "([], size=(0, 6))":
+    print("No bounding boxes found")
+    bbs = []
+else:
+    bbs = ast.literal_eval(response_str)
 image = cv2.imread(sample_image_path)
 
 # Confidence threshold in percent
@@ -35,10 +39,10 @@ for bb in bbs:
     x2 = int(bb[2])
     y2 = int(bb[3])
     class_id = int(bb[5])
-    conf = int(bb[4]*100)
+    conf = int(bb[4] * 100)
     if conf < conf_thres:
         continue
-    label = f'class: {class_id}, conf: {conf}%'
+    label = f"class: {class_id}, conf: {conf}%"
     cv2.rectangle(image, (x1, y1), (x2, y2), (255, 0, 0), 5)
     cv2.putText(image, label, (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 0, 0), 1)
 
