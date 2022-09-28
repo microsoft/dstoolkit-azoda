21a9db573f3dc1580c0b89422fe2479fb0459548
Daniel Baumann <ndebaumann@gmail.com>
2022-09-28 14:46:29 +0200

    Fix pipeline to get latest annotations from custom vision

--- src/auto_setup/create_dataset_splits.py
+++ src/auto_setup/create_dataset_splits.py
@@ -1,44 +1,24 @@
-# Script to convert current annotation style to the yolo annotation convention
-# %%
+# Script to partition a csv annotation file into train and test splits
+
 # Imports
 import argparse
-from PIL import Image
-import os
 import pandas as pd
-import shutil
 import util
 
-# parser = argparse.ArgumentParser()
-# parser.add_argument("-d", "--dataset", help="Name of dataset/project")
-# args = parser.parse_args()
-# dataset_name = args.dataset
-dataset_name = "synthetic_dataset"
-datasets_dir = "."
-dataset_path = util.get_lastest_iteration(
-    datasets_dir, req_prefix=f"all_{dataset_name}"
-)
+parser = argparse.ArgumentParser()
+parser.add_argument("-d", "--dataset", help="Name of dataset/project")
+args = parser.parse_args()
+dataset_name = args.dataset
 
-# %%
-print(dataset_path)
-# %%
+dataset_path = util.get_lastest_iteration(".", req_prefix=f"all_{dataset_name}")
 df = pd.read_csv(dataset_path)
-
-# %%
 train_test_split = 0.8
-# %%
 df_train = df[: round(train_test_split * df.shape[0])]
 df_test = df[round(train_test_split * df.shape[0]) :]
-# %%
-df_train
-# %%
-df_test
-# %%
+
 df_train.to_csv(
     dataset_path.replace(f"all_{dataset_name}", f"train_{dataset_name}"), index=False
 )
 df_test.to_csv(
     dataset_path.replace(f"all_{dataset_name}", f"test_{dataset_name}"), index=False
 )
-# %%
-dataset_path
-# %%
