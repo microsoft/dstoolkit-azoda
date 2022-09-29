519d28d65db0e087b4cac75b8c82292f2c8a1a61
Daniel Baumann <ndebaumann@gmail.com>
2022-09-29 13:06:35 +0200

    Connect import and export pipelines, add multi label support

--- src/auto_setup/util.py
+++ src/auto_setup/util.py
@@ -1,9 +1,13 @@
 import os
 
 
-def get_lastest_iteration(base_dir, req_prefix=''):
-    filenames = [f for f in os.listdir(base_dir)
-                 if os.path.isfile(os.path.join(base_dir, f))
-                 and f.startswith(req_prefix)]
-    print('filenames', filenames)
+def get_lastest_iteration(base_dir, req_prefix=""):
+    filenames = [
+        f
+        for f in os.listdir(base_dir)
+        if os.path.isfile(os.path.join(base_dir, f)) and f.startswith(req_prefix)
+    ]
+    print("filenames", filenames)
+    if not filenames:
+        return ""
     return os.path.join(base_dir, sorted(filenames)[-1])
