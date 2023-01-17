26b59bf426e83c721307e884c62116ecf27e5779
Daniel Baumann <ndebaumann@gmail.com>
2023-01-17 18:11:06 +0100

    Adds yolov7 interface files

---
+++ model_zoo/wongkinyiu_yolov7/score_gpu.py
@@ -0,0 +1,27 @@
+from io import BytesIO
+from PIL import Image
+import json
+import base64
+import torch
+import os
+
+
+def init():
+    global model
+    model = torch.hub.load(
+        "ultralytics_yolov5/yolov5",
+        "custom",
+        path="ultralytics_yolov5/best.pt",
+        source="local",
+        force_reload=True,
+        device=0,
+    )
+
+
+def run(request):
+    json_load = json.loads(request)
+    decoded_img = base64.b64decode(json_load["img"])
+    stream = BytesIO(decoded_img)
+    img = Image.open(stream).convert("RGBA")
+    results = model([img])
+    return f"{results.xyxy[0]}"
