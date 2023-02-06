4c0bf8a15d23a54dd972e578b1f6d9b94fe0a400
Ben Constable <benconstable@microsoft.com>
2023-02-06 13:39:45 +0000

    Format with black

--- model_zoo/ultralytics_yolov5/train_coordinator.py
+++ model_zoo/ultralytics_yolov5/train_coordinator.py
@@ -8,22 +8,24 @@ parser.add_argument("--batch-size", type=int, help="Batch size")
 parser.add_argument("--epochs", type=int, help="Number of epochs")
 args = parser.parse_args()
 
-print('Editing config')
+print("Editing config")
 # Updates the dataset location
-with open(f'{args.dataset}.yaml') as f:
+with open(f"{args.dataset}.yaml") as f:
     lines = f.readlines()
 print(lines)
-lines[0] = f'path: {args.dataset}/yolo\n'
-print('After edit:')
+lines[0] = f"path: {args.dataset}/yolo\n"
+print("After edit:")
 print(lines)
-with open(f'{args.dataset}.yaml', 'w') as f:
+with open(f"{args.dataset}.yaml", "w") as f:
     f.writelines(lines)
 
 # project must be set to outputs, since the AML saves results in the outputs directory
-print('Start training')
-os.system(f"python yolov5/train.py \
+print("Start training")
+os.system(
+    f"python yolov5/train.py \
           --batch-size {args.batch_size} \
           --cfg {args.cfg} \
           --data {args.dataset}.yaml \
           --epochs {args.epochs} \
-          --project outputs/")
+          --project outputs/"
+)
